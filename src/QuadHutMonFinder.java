8b91a2fe5bf251b52d29e2c6f0cfa3efb7b5b101
Erik Günther <erik.gunther@gmail.com>
2016-02-13 12:32:12 +0100

    remove debug

--- src/QuadHutMonFinder.java
+++ src/QuadHutMonFinder.java
@@ -177,7 +177,7 @@ public class QuadHutMonFinder {
 		while (startSeed < -281474976710656L || startSeed > 281470000000000L ) {
 			startSeed = rnd.nextLong(); //Long.parseLong(args[0]);	
 		} 
-		startSeed = -96928167637521L;
+		//debug startSeed = -96928167637521L;
 		System.out.println(startSeed);
 		long endSeed = 281474976710656L; //higher than 2^48 will be useless
 		int radius = 4;
